3e2eac1118dc652eae631c45106811fd63605ac0
matt-raines <mattraines.nz@gmail.com>
2017-05-26 10:30:41 +1200

    template html

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,2 +1,21 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
+
+export default class App extends React.Component {
+  constructor () {
+    super (props)
+  }
+
+  render () {
+    return (
+      <div>
+        <div id='title'>Title</div>
+        <form id='searchBar'>
+          <input type="text" id='search' placeholder="Search" />
+          <button id='btn'>Button</button>
+        </form>
+        <div id='textbox'>Textbox</div>
+      </div>
+    )
+  }
+}
