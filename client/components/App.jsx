233cbf6247a54bb802f009bc5098578f1ce6d8e7
albie-feary <laurabeth.feary@gmail.com>
2017-05-26 10:23:25 +1200

    title rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,2 +1,19 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+   render(){
+     return(
+       <div>
+         <h1 className = 'title' >Kanye-Rest</h1>
+       </div>
+     )
+   }
+
+
+}
